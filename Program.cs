0a0b0587d63d034624ffe8ab902061da8d43e249
Cysia15 <mixu12anaix8@gmail.com>
2024-10-24 09:28:14 +0200

    zrobilismy program ktory sprawdza ktora liczba jest wieksza i nauczylismy sie czym jest TryParse i gdzie go uzywac

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿class Klaskamj
+{
+
+    static void Main(string[] args)
+    {
+        int liczba1, liczba2;
+        Console.WriteLine("podaj liczbe");
+        bool czylok = int.TryParse(Console.ReadLine(), out liczba1);
+
+        Console.WriteLine("podaj druga liczbe ");
+        bool czyl2ok = int.TryParse(Console.ReadLine(),out liczba2);
+
+        if (liczba1 > liczba2)
+        {
+            Console.WriteLine($"ziomek liczba  {liczba1} jest wieksza");
+        }
+        else if (liczba1 < liczba2)
+        {
+            Console.WriteLine($"ziomek liczba  {liczba2} jest wieksza");
+        }
+        else
+        {
+            Console.WriteLine($"ziomek liczba  {liczba1} jest rowna");
+        }
+
+    }
+}
